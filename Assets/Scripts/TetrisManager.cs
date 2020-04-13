8c3fa7b72a81e57e46a17f54fc63b1ad22e5e8bb
LocketGoma <nothappa@naver.com>
2020-04-13 21:51:38 +0900

    게임오버 구현

--- Assets/Scripts/TetrisManager.cs
+++ Assets/Scripts/TetrisManager.cs
@@ -14,15 +14,17 @@ public class TetrisManager : MonoBehaviour
     [SerializeField] private GameObject secondTetrimino = null;
 
     [Header("자체 변수")]
-    [Range(0.0f,1.0f)]
+    [Range(0.0f, 1.0f)]
     [SerializeField] private float batchGap = 0.7f;
     [SerializeField] private int[] mapSize = { 9, 20 };
+    [SerializeField] private int[] batchPosition = { 5, 15 };
 
     [Header("조작 컨트롤러")]
     [SerializeField] private TetrisController tetrisController = null;
     [SerializeField] private bool endOfMove = false;
+    [SerializeField] private bool playState = true;
 
-    
+
 
     private void Start() {
         dataMatrix = new Transform[mapSize[0], mapSize[1]];
@@ -43,28 +45,32 @@ public class TetrisManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(endOfMove == true) { //현재 테트리미노가 사용불가가 되었을시
+        if (endOfMove == true) { //현재 테트리미노가 사용불가가 되었을시
             Debug.Log("Next");
             foreach (Transform child in nowTetrimino.transform)
             {
                 Vector2 v = roundVec2(child.position);
                 dataMatrix[(int)v.x, (int)v.y].name = "Rock";
+                playState = CheckOverflow(v) == false;
             }
-            nowTetrimino.GetComponent<Tetrimino>().StopMove();
 
-            nowTetrimino = nextTetrimino = Instantiate(baseTetrimino, new Vector2(0, 15), transform.rotation);
-            nowTetrimino.GetComponent<Tetrimino>().Initializer(batchGap);
-            //nowTetrimino = nextTetrimino;
-            //nowTetrimino.transform.position = new Vector2(0, 15);
-            //nextTetrimino = secondTetrimino;
-            //nextTetrimino.transform.position = new Vector2(5, 16);
-            //secondTetrimino = Instantiate(baseTetrimino, new Vector2(5, 2), transform.rotation);
-            //secondTetrimino.GetComponent<Tetrimino>().Initializer(batchGap);
+                nowTetrimino.GetComponent<Tetrimino>().StopMove();
+            if (playState == true)
+            {
+                nowTetrimino = nextTetrimino = Instantiate(baseTetrimino, new Vector2(0, 15), transform.rotation);
+                nowTetrimino.GetComponent<Tetrimino>().Initializer(batchGap);
+                //nowTetrimino = nextTetrimino;
+                //nowTetrimino.transform.position = new Vector2(0, 15);
+                //nextTetrimino = secondTetrimino;
+                //nextTetrimino.transform.position = new Vector2(5, 16);
+                //secondTetrimino = Instantiate(baseTetrimino, new Vector2(5, 2), transform.rotation);
+                //secondTetrimino.GetComponent<Tetrimino>().Initializer(batchGap);
 
-            tetrisController.Tetrimino = nowTetrimino.GetComponent<Tetrimino>();
-            nowTetrimino.GetComponent<Tetrimino>().CanMove();
+                tetrisController.Tetrimino = nowTetrimino.GetComponent<Tetrimino>();
+                nowTetrimino.GetComponent<Tetrimino>().CanMove();
 
-            endOfMove = false;
+                endOfMove = false;
+            }
         }
         UpdateData();
     }
@@ -76,7 +82,7 @@ public class TetrisManager : MonoBehaviour
             Destroy(dataMatrix[x, y].gameObject);
             dataMatrix[x, y] = null;
         }
-        DecreaseRow(y+1);
+        DecreaseRow(y + 1);
     }
     void DecreaseRow(int y) {
         bool transCheck = false;
@@ -92,8 +98,7 @@ public class TetrisManager : MonoBehaviour
             }
         }
         if (transCheck == true)
-        DecreaseRow(y + 1);
-
+            DecreaseRow(y + 1);
     }
 
     void UpdateData() {
@@ -113,12 +118,20 @@ public class TetrisManager : MonoBehaviour
             Debug.Log("now Check :" + v.x + "," + v.y);
         }
         Debug.Log("-----------");
-    } 
+    }
     public static Vector2 roundVec2(Vector2 v) {
         return new Vector2(Mathf.Round(v.x),
                            Mathf.Round(v.y));
     }
-
+    //꽉 차서 게임 종료 되는 경우.
+    bool CheckOverflow(Vector2 checker)
+    {
+        if (checker.y >= batchPosition[1]) { 
+            return true;
+        } else {
+        return false;
+        }
+    }
 
 
 }
